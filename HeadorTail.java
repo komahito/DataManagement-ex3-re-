b271f467c8db780f164ecfdc1fc473e5eb3c2868
komahito <hitoha.koma@gmail.com>
2024-07-04 21:24:57 +0900

    HoT: initial revision

---
+++ HeadorTail.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+class HeadorTail {
+    public static void main (String[] args) {
+        Random rand = new Random();
+        int headCnt = 0, tailCnt = 0;
+        System.out.println("Tossing a coin...");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Round "+ (i + 1) +": ");
+            int num = rand.nextInt(10) + 100;
+            if (num % 2 == 0) {
+                headCnt++;
+                System.out.println("Heads");
+            } else {
+                tailCnt++;
+                System.out.println("Tails");
+            }
+        }
+        System.out.println("Head: "+headCnt+", "+"Tails: "+tailCnt);
+    }
+}
